358bd3811a7b86aaef84423bc6d2d1592494c1b0
ryan mapa <ryan.m.mapa@gmail.com>
2017-11-19 18:18:03 -0800

    create restful routes for tasks

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  get 'tasks/create'
 
-  get 'tasks/destroy'
-
-  get 'tasks/index'
-
-  get 'tasks/edit'
-
-  get 'tasks/show'
-
+  resources :tasks, only: %i(create destroy index edit show)
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
