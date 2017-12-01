4003b3b6eab254317807f9c64ec3bcdc57e86fa3
ryan mapa <ryan.m.mapa@gmail.com>
2017-12-01 15:34:32 -0800

    Create ui and entities reducers

---
+++ frontend/src/reducers/entities_reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import TasksReducer from './tasks_reducer';
+
+const EntitiesReducer = combineReducers({
+  tasks: TasksReducer
+});
+
+export default EntitiesReducer;
