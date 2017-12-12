9dd2aa080c3015346c0085bf52c28eadbff46e33
David <davidgbanks91@gmail.com>
2017-12-11 18:09:10 -0800

    Install react-native-elements library and implement checkboxes for Task index items

--- frontend/src/components/tasks/task_index.js
+++ frontend/src/components/tasks/task_index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { CheckBox } from 'react-native-elements';
 
 class TaskIndex extends React.Component {
   constructor(props) {
@@ -22,6 +23,11 @@ class TaskIndex extends React.Component {
     return (
       <View>
         <Text>Hi {this.props.user.first_name}</Text>
+          {
+            this.props.tasks.map(task => (
+              <CheckBox key={task.id} title={task.title} checked={task.completed} />
+            ))
+          }
       </View>
     );
   }
