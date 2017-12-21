b3663c84a4aab6889d2705c71ab8958e1453cb42
David <davidgbanks91@gmail.com>
2017-12-21 00:42:07 -0800

    Install checkbox on TaskShow

--- frontend/src/components/tasks/task_show.js
+++ frontend/src/components/tasks/task_show.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { CheckBox } from 'react-native-elements';
 
 class TaskShow extends React.Component {
   constructor(props) {
     super(props);
-    console.log('Inside TaskShow: props', props);
+    this.toggleCompleted = this.toggleCompleted.bind(this);
+  }
+
+  toggleCompleted(task) {
+    return () => {
+      task.completed = !task.completed;
+      this.props.editTask(task);
+    };
   }
 
   render() {
+    const task = this.props.task;
     return (
       <View>
-        <Text>Hellow out there</Text>
+        <Text>{task.title}</Text>
+          <CheckBox
+            title={<Text>Completed?</Text>}
+            checked={task.completed}
+            onIconPress={ this.toggleCompleted(task) } />
       </View>
     );
   }
--- frontend/src/components/tasks/task_show_container.js
+++ frontend/src/components/tasks/task_show_container.js
@@ -1,10 +1,13 @@
 import { connect } from 'react-redux';
+import { editTask } from '../../actions/task_actions';
 import TaskShow from './task_show';
 
 const mapStateToProps = (state, ownProps) => ({
   task: state.entities.tasks[ownProps.navigation.state.params.taskId]
 });
 
-const mapDispatchToProps = null;
+const mapDispatchToProps = dispatch => ({
+  editTask: task => dispatch(editTask(task))
+});
 
-export default connect(mapStateToProps, null)(TaskShow);
+export default connect(mapStateToProps, mapDispatchToProps)(TaskShow);
