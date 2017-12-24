c77231290ce16d3984ff1f6c154ec993e2c8fd9f
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2017-12-24 14:04:59 -0800

    Separate loading spinner into its own component

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -2,31 +2,13 @@ import React from 'react';
 import { AsyncStorage,
          Text,
          View,
-         StyleSheet,
-         Animated,
-         Image,
-         Easing } from 'react-native';
+         StyleSheet
+       } from 'react-native';
+import Loading from '../loading/loading';
 
 class Splash extends React.Component {
   constructor(props) {
     super(props);
-    this.spinValue = new Animated.Value(0);
-  }
-
-  componentDidMount() {
-    this.spin();
-  }
-
-  spin() {
-    this.spinValue.setValue(0);
-    Animated.timing(
-      this.spinValue,
-      {
-        toValue: 1,
-        duration: 1200,
-        easing: Easing.linear
-      }
-    ).start(() => this.spin());
   }
 
   componentWillMount() {
@@ -45,26 +27,10 @@ class Splash extends React.Component {
   }
 
   render() {
-    const spin = this.spinValue.interpolate({
-      inputRange: [0, 1],
-      outputRange: ['0deg', '360deg']
-    });
-
     return (
       <View style={ styles.container }>
         <Text style={ styles.title }>Task Academy</Text>
-        <Animated.Image
-          style={
-            {
-              width: 100,
-              height: 100,
-              transform: [{ rotate: spin }],
-              alignSelf: 'center',
-              padding: 50
-            }
-          }
-          source={ { uri: 'https://d30y9cdsu7xlg0.cloudfront.net/png/17406-200.png' } }
-          />
+        <Loading />
       </View>
     );
   }
