f2589ef339c1804acbda2f8105ecd322561fd1bc
David <davidgbanks91@gmail.com>
2017-12-26 12:32:04 -0800

    Remove useless imports from Routers

--- frontend/src/routers.js
+++ frontend/src/routers.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Icon, Button } from 'react-native-elements';
-import { StackNavigator, NavigationActions } from 'react-navigation';
+import { StackNavigator } from 'react-navigation';
 import SplashContainer from './components/splash/splash_container';
 import LoginContainer from './components/login/login_container';
 import TaskIndexContainer from './components/tasks/task_index_container';
