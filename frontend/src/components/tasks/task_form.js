aff85c17e2c7c56bc07151b42956708f3809c2ee
David <davidgbanks91@gmail.com>
2017-12-27 08:25:04 -0800

    Install DatePickerIOS

--- frontend/src/components/tasks/task_form.js
+++ frontend/src/components/tasks/task_form.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, DatePickerIOS } from 'react-native';
 import {
   FormLabel,
   FormInput,
   FormValidationMessage,
+  CheckBox,
   Button
 } from 'react-native-elements';
 
@@ -11,34 +12,73 @@ class TaskForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = { owner_id: props.userId };
-    this.placeholders = {
-      title: 'Enter a title ...'
-    }
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentWillMount() {
-    console.log('task form mounting');
     if (this.props.task) {
-      console.log('TASK UP IN HERE', this.props.task);
       const task = this.props.task;
-      this.setState({ title: task.title });
+      this.setState({
+        title: task.title,
+        due_date: task.due_date,
+        parent_id: task.parent_id
+       });
+    }
+  }
+
+  handleSubmit() {
+    if (this.props.task) {
+      this.props.editTask(this.state);
+    } else {
+      this.props.createTask(this.state);
+    }
+  }
+
+  displayDatePicker() {
+    if (this.state.due_date) {
+      return (
+        <DatePickerIOS
+          mode={'date'}
+          date={this.state.due_date}
+          onDateChange={(date) => this.setState({due_date: date})} />
+      );
     }
   }
 
   render() {
-    console.log('taskform: render', this.state);
+    console.log(this.state);
     return (
       <View>
-        <Text>INSIDE THE TASK FORM</Text>
         <View>
           <FormLabel>Title</FormLabel>
           <FormValidationMessage>Required</FormValidationMessage>
-          <FormInput value={this.placeholders.title}/>
+          <FormInput
+            placeholder={'Enter a title ...'}
+            onChangeText={(text) => this.setState({title: text})} />
         </View>
-        <Button title='Save' />
+
+        <View>
+          <CheckBox
+            title={ 'Does this have a deadline?' }
+            checked={ Boolean(this.state.due_date) }
+            onIconPress={ () => this.state.due_date ?
+              this.setState({due_date: null}) :
+              this.setState({due_date: new Date()}) } />
+
+          {this.displayDatePicker()}
+        </View>
+
+        <Button
+          small
+          backgroundColor='green'
+          icon={ { name: 'save' } }
+          title='Save'
+          onPress={ () => this.handleSubmit() } />
       </View>
     );
   }
 }
 
 export default TaskForm;
+
+// Tasks/app/components/EditTask/index.js
