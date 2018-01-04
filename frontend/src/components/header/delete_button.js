73b0d0a33f15e6bb3d552532c44a7120118d95da
David <davidgbanks91@gmail.com>
2018-01-04 15:17:02 -0800

    Add condition for delete button rendering

--- frontend/src/components/header/delete_button.js
+++ frontend/src/components/header/delete_button.js
@@ -5,19 +5,24 @@ import { Icon } from 'react-native-elements';
 class DeleteButton extends React.Component {
   constructor(props) {
     super(props);
+    console.log('DeleteButton.props=', props);
   }
 
   render() {
-    return (
-      <View>
-        <Icon
-          name='delete'
-          size={30}
-          color='red'
-          containerStyle={{marginRight: 20}}
-          onPress={ () => this.props.navigation.navigation.setParams({delete:true}) } />
-      </View>
-    );
+    if (this.props.navigation.navigation.state.params.task) {
+      return (
+        <View>
+          <Icon
+            name='delete'
+            size={30}
+            color='red'
+            containerStyle={{marginRight: 20}}
+            onPress={ () => this.props.navigation.navigation.setParams({delete:true}) } />
+        </View>
+      );
+    } else {
+      return <View></View>;
+    }
   }
 }
 
