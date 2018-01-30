1753d87e637ada5ab9ac7cf3bc69403d66f6f4aa
ryan mapa <ryan.m.mapa@gmail.com>
2018-01-29 17:19:32 -0800

    Connect to heroku backend and remove back bug on splash and login

--- frontend/src/util/user_api_util.js
+++ frontend/src/util/user_api_util.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 export const postUser = user => (
-  axios.post('http://localhost:3000/api/users/login',
+  axios.post('https://task-academy.herokuapp.com/api/users/login',
   { user })
 );
 
 export const getUser = sessionToken => (
-  axios.get(`http://localhost:3000/api/users/${sessionToken}`)
+  axios.get(`https://task-academy.herokuapp.com/api/users/${sessionToken}`)
 );
